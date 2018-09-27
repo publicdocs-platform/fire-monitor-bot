7805e7515d5e2c5977795abf51d1ec7b3de9b617
Advay Mengle <source@madvay.com>
2018-09-26 20:53:34 -0700

    Filter out emerging fires with no info that are >3 hrs old

--- commands/run.js
+++ commands/run.js
@@ -164,6 +164,7 @@ exports.handler = argv => {
     for (let key in entry) {
       ret[key] = entry[key];
       if (key.endsWith('DateTime')) {
+        ret[key + 'Epoch'] = ret[key];
         ret[key] = dateString(ret[key]);
       }
       if (_.isString(ret[key])) {
@@ -474,6 +475,8 @@ exports.handler = argv => {
         KnownLocationLowPop: lat && lon && nearPopulation <= 1000,
         UnknownLocationSmallSize: !lat && !lon && (cur.DailyAcres || 0) < 1.1 && (cur.TotalIncidentPersonnel || 0) < 15,
         FalseAlarmName: cur.Fire_Name.toLowerCase().includes('false') && cur.Fire_Name.toLowerCase().includes('alarm'),
+        // 3 hours with no info, might be stale
+        OldEmergingFiresWithoutInfo: (cur.NFSAType || '').includes('Emerging') && !cur.DailyAcres && !cur.PercentContained && (cur.ModifiedOnDateTimeEpoch - cur.FireDiscoveryDateTimeEpoch > 1000 * 60 * 60 * 3),
       }
 
       for (let filterKey in displayFilters) {
