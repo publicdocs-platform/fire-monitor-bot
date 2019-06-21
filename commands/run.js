7f8b243f2c97106d3bf89877bc820bed5ce044a2
Advay Mengle <advayDev1@users.noreply.github.com>
2019-06-21 01:29:04 -0700

    Take the most up to date perimeter from GEOMAC Latest and Current perim layers (#45)
    * Take the most up to date perimeter from GEOMAC Latest and Current perim layers
    
    * Log errors correctly; check for perim. acre changes
    
    * Fix maprender refactor
    
    * Fix pruning logic

--- commands/run.js
+++ commands/run.js
@@ -300,7 +300,17 @@ exports.handler = (argv) => {
       fs.writeFileSync(argv.outputdir + '/data/GLOBAL-DIFF-' + globalUpdateId + '.yaml', diffsGlobal);
     }
 
-    const perims = await geomac.getPerimeters(argv.userAgent);
+    const perims1 = await geomac.getPerimeters(argv.userAgent, false);
+    const perims2 = await geomac.getPerimeters(argv.userAgent, true);
+    const perims = {};
+    const perimKeys = _.union(_.keys(perims1), _.keys(perims2));
+    perimKeys.map((key) => {
+      const merged = util.latestPerimeter(perims1[key], perims2[key]);
+      perims[key] = merged;
+      if (!merged) {
+        logger.warn('Missing perim ' + key);
+      }
+    });
 
     const xkeys = _.keys(x);
     const xsortedKeys = _.sortBy(xkeys, (i) => -x[i].DailyAcres);
@@ -319,25 +329,27 @@ exports.handler = (argv) => {
         // Keep the newer data around.
         x[i] = Object.assign({}, last[i]);
         x[i].PerimDateTime = perimDateTime;
+        x[i].PerimeterData = cur.PerimeterData;
         cur = x[i];
-
-        if (!cur.ModifiedOnDateTimeEpoch || cur.ModifiedOnDateTimeEpoch < pruneTime) {
-          logger.info(' #! Pruning %s %s -> last mod %s', i, cur.Name, cur.ModifiedOnDateTime);
-          delete x[i];
-          continue;
-        }
 
         // Only skip the update if perimeter is ALSO not up to date.
         if (!perimDateTime || (last[i].PerimDateTime && last[i].PerimDateTime >= perimDateTime)) {
+          x[i].PerimDateTime = last[i].PerimDateTime;
+          x[i].PerimeterData = last[i].PerimeterData;
           continue;
         }
       }
 
+      if (!cur.ModifiedOnDateTimeEpoch || cur.ModifiedOnDateTimeEpoch < pruneTime) {
+        logger.info(' #! Pruning %s %s -> last mod %s', i, cur.Name, cur.ModifiedOnDateTime);
+        delete x[i];
+        continue;
+      }
 
       let oneDiff = deepDiff(old, cur);
       oneDiff = _.keyBy(oneDiff, (o) => o.path.join('.'));
 
-      if (!('DailyAcres' in oneDiff || 'PercentContained' in oneDiff)) {
+      if (!('DailyAcres' in oneDiff || 'PercentContained' in oneDiff || 'PerimeterData.Acres' in oneDiff)) {
         if (!argv.monitorPerims || !('PerimDateTime' in oneDiff)) {
           // Unless acreage, perim, or containment change, we don't report it.
           continue;
@@ -347,7 +359,7 @@ exports.handler = (argv) => {
           continue;
         }
       }
-      if (!('PercentContained' in oneDiff || 'PerimDateTime' in oneDiff) && old.DailyAcres && cur.DailyAcres && Math.abs(cur.DailyAcres - old.DailyAcres) < 1.1) {
+      if (!('PercentContained' in oneDiff || 'PerimeterData.Acres' in oneDiff) && old.DailyAcres && cur.DailyAcres && Math.abs(cur.DailyAcres - old.DailyAcres) < 1.1) {
         // May be spurious - due to rounding in GEOMAC vs NFSA.
         continue;
       }
@@ -375,7 +387,8 @@ exports.handler = (argv) => {
       try {
         await internalProcessFire(logger, updateId, inciWeb, cur, perim, old, oneDiff, isNew, key, perimDateTime);
       } catch (err) {
-        logger.error('$$$$ ERROR processing %s', updateId, err);
+        logger.error('$$$$ ERROR processing %s', updateId);
+        logger.error(err);
       } finally {
         intensiveProcessingSemaphore.leave();
       }
@@ -387,7 +400,8 @@ exports.handler = (argv) => {
       try {
         await exec(argv.postPersistCmd);
       } catch (err) {
-        logger.error('### Error in post persist command: ', err);
+        logger.error('### Error in post persist command: ');
+        logger.error(err);
       }
     }
 
@@ -418,7 +432,7 @@ exports.handler = (argv) => {
       }
       let rr = null;
       if (perim.length > 1 && argv.locations) {
-        rr = await maprender.renderMap(null, perim, 1450 / 2, 1200 / 2, 15, true);
+        rr = await maprender.getMapBounds(perim, 1450 / 2, 1200 / 2, 15);
       } else {
         logger.info('>> Missing perimeter - %s', updateId);
       }
@@ -511,7 +525,6 @@ exports.handler = (argv) => {
         isNew: isNew,
         mapData: {events: events},
         terrainImg: terrainImg,
-        terrainCredit: terrainImg ? maprender.terrainCredit : '',
       };
       const html = genHtml(templateData);
       const tweet = genTweet(templateData);
@@ -531,14 +544,16 @@ exports.handler = (argv) => {
             lon: lon,
             zoom: zoom,
             cities: displayCities,
-            mapData: {events: events},
+            mapData: {
+              events: events,
+              perimSourceLayer: cur.PerimeterData ? cur.PerimeterData._Provenance.SourceLayer : null,
+            },
             perimDateTime: perimDateTime,
             current: cur,
             last: old,
             diff: oneDiff,
             isNew: isNew,
             img: detailImg,
-            imgCredit: maprender.detailedCredit,
           };
           const htmlPerim = perimeterHtml(perimTemplateData);
           fs.writeFileSync(perimWebpage, htmlPerim);
@@ -592,32 +607,43 @@ exports.handler = (argv) => {
     const i = key;
     const cur = x[i];
     const old = last[i] || {};
+    let perimAcres = null;
     let perim = [];
     let perimDateTime = null;
     let inciWeb = null;
+    let perimProvenance = null;
     if (cur.UniqueFireIdentifier in perims) {
-      perim = perims[cur.UniqueFireIdentifier].geometry.coords || [];
-      perimDateTime = perims[cur.UniqueFireIdentifier].attributes.perimeterdatetime;
-      inciWeb = perims[cur.UniqueFireIdentifier].attributes.inciwebid;
+      const p = perims[cur.UniqueFireIdentifier];
+      perim = p.geometry.coords || [];
+      perimDateTime = p.attributes.perimeterdatetime;
+      inciWeb = p.attributes.inciwebid;
+      perimAcres = p.attributes.gisacres;
+      perimProvenance = p._Provenance;
     }
     const children = _.values(perims)
-        .filter((fire) => {
-          const b = (fire.attributes.complexname || '').toLowerCase() === (cur.Fire_Name || '').toLowerCase();
+        .filter((p) => {
+          const b = (p.attributes.complexname || '').toLowerCase() === (cur.Fire_Name || '').toLowerCase();
           if (b) {
-            if (!perimDateTime || perimDateTime < fire.attributes.perimeterdatetime) {
-              perimDateTime = fire.attributes.perimeterdatetime;
-              inciWeb = fire.attributes.inciwebid;
+            if (!perimDateTime || perimDateTime < p.attributes.perimeterdatetime) {
+              perimDateTime = p.attributes.perimeterdatetime;
+              inciWeb = p.attributes.inciwebid;
             }
           }
           return b;
         })
-        .map((fire) => fire.geometry.coords)
+        .map((p) => p.geometry.coords)
         .reduce((a, b) => a.concat(b), []);
     perim = perim.concat(children);
     if (cur.Lat) {
       perim.push([[[cur.Lon, cur.Lat], [cur.Lon, cur.Lat]]]);
     }
+    // TODO: Migrate to PerimeterData.
     cur.PerimDateTime = perimDateTime;
+    cur.PerimeterData = {
+      DateTime: perimDateTime,
+      Acres: perimAcres,
+      _Provenance: perimProvenance,
+    };
     cur.unitId = cur.pooresponsibleunit || cur.UniqueFireIdentifier.split('-')[1];
     cur.unitMention = units.unitTag(cur.unitId);
     return {i, cur, perimDateTime, old, inciWeb, perim};
@@ -635,7 +661,8 @@ exports.handler = (argv) => {
           await dailyMap();
           x = await internalLoop(first, last);
         } catch (err) {
-          logger.error('>> Main loop error', err);
+          logger.error('>> Main loop error');
+          logger.error(err);
         } finally {
         }
 
